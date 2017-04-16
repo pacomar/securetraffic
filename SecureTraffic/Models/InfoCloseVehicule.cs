7a1033b7567d9c1d26eb29f4d65a16d3db9b1dd3
WorkStation <WorkStation@DESKTOP-AFNE4CH>
2017-04-16 18:31:45 +0200

    Subir cambios sin probar

---
+++ SecureTraffic/Models/InfoCloseVehicule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecureTraffic.Models
+{
+    public class InfoCloseVehicule
+    {
+        public int distancia { get; set; } = int.MaxValue;
+
+        public int tiempo { get; set; } = int.MaxValue;
+
+        public string direccionVehiculoLento { get; set; } = "";
+
+        public string direccionVehiculoPropio { get; set; } = "";
+    }
+}
--- SecureTraffic/ViewModels/FastVehicleViewModel.cs
+++ SecureTraffic/ViewModels/FastVehicleViewModel.cs
@@ -4,62 +4,144 @@ using System.Diagnostics;
 using System.Threading.Tasks;
 using Plugin.Geolocator;
 using Xamarin.Forms.Maps;
+using Firebase.Xamarin.Database;
+using SecureTraffic.Models;
+using Org.Json;
+using System.Net.Http;
 
 namespace SecureTraffic
 {
-	public class FastVehicleViewModel
-	{
-		private Map _map { get; set; } 
-		public FastVehicleViewModel(Map _map)
-		{
-			this._map = _map;
-			CenterMap();
-			UpdateMarkers();
-		}
+    public class FastVehicleViewModel
+    {
+        private Map _map { get; set; }
+        private Position myPosition;
 
-		public async Task<bool> CenterMap()
-		{
-			bool res = false;
+        public FastVehicleViewModel(Map _map)
+        {
+            this._map = _map;
+            CenterMap();
+            UpdateMarkers();
+        }
 
-			try
-			{
-				var locator = CrossGeolocator.Current;
-				locator.DesiredAccuracy = 50;
+        public async Task<bool> CenterMap()
+        {
+            bool res = false;
 
-					var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
-					if (position == null)
-						return res;
+            try
+            {
+                var locator = CrossGeolocator.Current;
+                locator.DesiredAccuracy = 50;
 
-				this._map.MoveToRegion(new MapSpan(new Position(position.Latitude, position.Longitude), 0.05, 0.05));
+                var position = await locator.GetPositionAsync(timeoutMilliseconds: 10000);
+                if (position == null)
+                    return res;
 
-				res = true;
-			}
-			catch (Exception ex)
-			{
-				Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
-			}
+                myPosition = new Position(position.Latitude, position.Longitude);
 
-			return res;
-		}
+                this._map.MoveToRegion(new MapSpan(new Position(position.Latitude, position.Longitude), 0.05, 0.05));
 
-		public async Task<bool> UpdateMarkers()
-		{
-			VehiclesService _vehServ = new VehiclesService();
-			var vehicles = await _vehServ.GetVehicles();
+                res = true;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Unable to get location, may need to increase timeout: " + ex);
+            }
 
-			foreach (var vehicle in vehicles)
-			{
-				var pin = new Pin
-				{
-					Type = PinType.Place,
-					Position = new Position(vehicle.Object.Latitude, vehicle.Object.Longitude),
-					Label = "slow vehicle",
-					Address = "custom detail info"
-				};
-				this._map.Pins.Add(pin);
-			}
+            return res;
+        }
 
-			return true;
-		}
-	}
+        public async Task<bool> UpdateMarkers()
+        {
+            VehiclesService _vehServ = new VehiclesService();
+            var vehicles = await _vehServ.GetVehicles();
+
+            foreach (var vehicle in vehicles)
+            {
+                string address = "Too far";
+
+                if (CalculateDistanceLine(myPosition.Latitude, myPosition.Longitude, vehicle.Object.Latitude, vehicle.Object.Longitude) < 1000)
+                {
+                    InfoCloseVehicule GetInformationCloseVehicle = await this.GetInformationCloseVehicle(myPosition.Latitude, myPosition.Longitude, vehicle.Object.Latitude, vehicle.Object.Longitude);
+                    address = GetInformationCloseVehicle.direccionVehiculoLento;
+                }
+
+                var pin = new Pin
+                {
+                    Type = PinType.Place,
+                    Position = new Position(vehicle.Object.Latitude, vehicle.Object.Longitude),
+                    Label = "slow vehicle",
+                    Address = address
+                };
+                this._map.Pins.Add(pin);
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Funcion que calcula la distancia en linea recta dadas 2 coordenadas (matemáticamente)
+        /// </summary>
+        /// <param name="fromLong">Longitud origen</param>
+        /// <param name="fromLat">Latitud origen</param>
+        /// <param name="toLong">Longitud destino</param>
+        /// <param name="toLat">Latitud destino</param>
+        /// <returns>Resultado en m de la distancia</returns>
+        private double CalculateDistanceLine(double fromLong, double fromLat,
+                    double toLong, double toLat)
+        {
+            double d2r = Math.PI / 180;
+            double dLong = (toLong - fromLong) * d2r;
+            double dLat = (toLat - fromLat) * d2r;
+            double a = Math.Pow(Math.Sin(dLat / 2.0), 2) + Math.Cos(fromLat * d2r)
+                    * Math.Cos(toLat * d2r) * Math.Pow(Math.Sin(dLong / 2.0), 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+            double d = 6367000 * c;
+            return Math.Round(d);
+        }
+
+        /// <summary>
+        /// Funcion que devuelve la informacion avanzada entre dos puntos
+        /// </summary>
+        /// <param name="fromLong">Longitud origen</param>
+        /// <param name="fromLat">Latitud origen</param>
+        /// <param name="toLong">Longitud destino</param>
+        /// <param name="toLat">Latitud destino</param>
+        /// <returns>Devuelve informacion de si estan en la misma carretera, </returns>
+        private async Task<InfoCloseVehicule> GetInformationCloseVehicle(double fromLong, double fromLat,
+                    double toLong, double toLat)
+        {
+            InfoCloseVehicule informacionVehiculo = new InfoCloseVehicule();
+            string url = "http://maps.googleapis.com/maps/api/directions/json?origin=" + fromLong + "," + fromLat + "&destination=" + toLong + ","  + toLat + "&sensor=false";
+
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(url))
+            using (HttpContent content = response.Content)
+            {
+                // ... Read the string.
+                string result = await content.ReadAsStringAsync();
+
+                // ... Display the result.
+                if (result != null)
+                {
+                    JSONObject json = new JSONObject(result);
+                    JSONArray routeArray = json.GetJSONArray("routes");
+                    JSONObject routes = routeArray.GetJSONObject(0);
+
+                    JSONArray newTempARr = routes.GetJSONArray("legs");
+                    JSONObject newDisTimeOb = newTempARr.GetJSONObject(0);
+
+                    JSONObject distOb = newDisTimeOb.GetJSONObject("distance");
+                    JSONObject timeOb = newDisTimeOb.GetJSONObject("duration");
+
+                    informacionVehiculo.distancia = Int32.Parse(distOb.GetString("value"));
+                    informacionVehiculo.tiempo = Int32.Parse(timeOb.GetString("value"));
+                    informacionVehiculo.direccionVehiculoLento = newDisTimeOb.GetString("value");
+                    informacionVehiculo.direccionVehiculoPropio = newDisTimeOb.GetString("value");
+
+                    return informacionVehiculo;
+                }
+            }
+            return informacionVehiculo;
+        }
+    }
 }
